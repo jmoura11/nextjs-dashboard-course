f1336f003866f944c88c372f4d53d9616d418178
jmoura11 <joaosanfinsmoura@gmail.com>
2024-01-02 18:30:57 +0000

    auth fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Acme Dashboard',
@@ -15,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}<Analytics /></body>
     </html>
   );
 }
